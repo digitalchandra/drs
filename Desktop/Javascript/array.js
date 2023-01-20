bbdb4eac19bf1638e3184441b1d24351054beb17
seoshrestha <chandra.shrestha@digitaltransformationsystem.com>
2023-01-20 21:58:28 +0545

    array

---
+++ Desktop/Javascript/array.js
@@ -0,0 +1,30 @@
+// let a =[5,4,3,6,4]
+// console.log(a[2]);
+
+//Array Methods 
+// 1. toSting 
+    // let num = [1,2,3,4,5]
+    // let a = num.toString()
+    // console.log(a)
+
+// 2. join 
+
+// let a = ['ram', 'shyam', 'hari', 'gita']
+// let b = a.join("+");
+// console.log(b)
+
+// 3. pop (it remove last array item this means pop method)
+
+// let a=[1,2,4,5,6,7,8]
+//  a.pop()
+// console.log(a)
+
+// Array Push (adding new element on last of array)
+// let b = [1,2,3,4,5,6]
+// let c =b.push(40);
+// console.log(b,c)
+
+//Array Shift (remove frist element and return )
+let a = [1,2,3,4,5,6]
+let b = a.shift()
+console.log(a,b)
