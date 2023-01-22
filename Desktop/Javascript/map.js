a4af1910ec43623f0d85569d3cf555861dbbcaec
seoshrestha <chandra.shrestha@digitaltransformationsystem.com>
2023-01-22 16:01:57 +0545

    map filer and reduce

---
+++ Desktop/Javascript/map.js
@@ -0,0 +1,23 @@
+// Map, filter , Reduce 
+
+// let a = [5,10,15,20]
+// let b= a.map((num, index)=>{
+//     console.log(num, index )
+// })
+// console.log(b)
+
+//Filter 
+
+// let a = [23,14,15,67,1,5,8,9]
+//  let b = a.filter((num)=>{
+//      return num>8
+//  })
+//  console.log(b)
+
+//Reduce 
+let a = [1,2,3,4]
+let b =a.reduce((num, num1)=>{
+    return num+num1
+    
+})
+console.log(b)
