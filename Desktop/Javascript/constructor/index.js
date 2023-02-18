55ba0737800d1335fecbf09e4f1bded6d4f45925
seoshrestha <chandra.shrestha@digitaltransformationsystem.com>
2023-02-18 22:29:00 +0545

    construcotr

---
+++ Desktop/Javascript/constructor/index.js
@@ -0,0 +1,16 @@
+class Laptop{
+    constructor(){
+        this.name = 'Mac book pro';
+        this.space = '256GB';
+        this.ram = '50GB';
+        this.screen = '16 inch';
+        this.price = ' USD 40000';
+
+    }
+}
+
+const laptop1 = new Laptop();
+const na = new Laptop()
+
+console.log(laptop1.name)
+console.log(na.name)
