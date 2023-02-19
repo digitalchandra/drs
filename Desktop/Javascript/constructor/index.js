f15bd0fce5344431b8ad725e0e7446643b8f1141
seoshrestha <chandra.shrestha@digitaltransformationsystem.com>
2023-02-19 11:20:27 +0545

    custroter added

--- Desktop/Javascript/constructor/index.js
+++ Desktop/Javascript/constructor/index.js
@@ -14,3 +14,17 @@ const na = new Laptop()
 
 console.log(laptop1.name)
 console.log(na.name)
+/// Constructor is the speical method for assign properties automatically call web object 
+class Car{
+    constructor(brand, model, seat, price){
+        this.brand = brand;
+        this.model = model;
+        this.seat = seat;
+        this.price = price;
+    }
+}
+let vehical = new Car("Mustang", "2023", " 4 sitter", " $ 4000");
+let vehical1 = new Car("Farari", "2022", "2 sitter", "$50000")
+
+console.log(vehical.brand)
+console.log(vehical.price)
