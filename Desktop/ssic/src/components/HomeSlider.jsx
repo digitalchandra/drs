95e5902bdf0af92561196c825eca6870a6998922
seoshrestha <chandra.shrestha@digitaltransformationsystem.com>
2023-08-31 21:33:29 +0545

    slider setting and API intregration

--- Desktop/ssic/src/components/HomeSlider.jsx
+++ Desktop/ssic/src/components/HomeSlider.jsx
@@ -4,13 +4,15 @@ import { useEffect } from 'react'
 import axios from 'axios';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import banner from '../images/website.jpg'
 import Slider from 'react-slick';
+import banner2 from '../images/banenr.jpg'
 
 export default function HomeSlider() {
     const [slider, setSlider] = useState()
 
     useEffect(()=>{
-        let url =('https://ptccollection.com/wp-json/wp/v2/slider')
+        let url =('https://ptccollection.com/wp-json/wl/v1/posts')
         axios.get(url).then((res)=>{
             setSlider(res.data)
         });
@@ -21,31 +23,27 @@ export default function HomeSlider() {
         infinite: true,
         speed: 500,
         slidesToShow: 1,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+        autoplay: true
       };
 
   return (
     <>
-   {
-       slider?.map((slider)=>{
-           return(
-               <>
-               <Slider {...settings}>
-                <div>
-               
-                    <p> {slider.title.rendered}</p>
-                  
-                    <img src='' />
-                
-                </div>
-                
-                </Slider>
-              
-               
-               </>
-           )
-       })
-   }
+      <Slider{...settings}> 
+    {
+        slider?.map((slider)=>{
+            return(
+                <>
+                    <div className="slider">
+                            <img src={slider.featured_image.large} alt="" className='img-fluid'/>
+                          
+                    </div>
+        
+                </>
+            )
+        })
+    }
+  </Slider>
     </>
   )
 }
